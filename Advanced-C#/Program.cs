5303b386adf8e23824fb48af38a4b6a6a0a6441e
ziad <ziadalimohammed812@gmail.com>
2025-01-23 02:30:26 +0200

    Add project files.

---
+++ Advanced-C#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advanced_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
